661f307c1159d79e004a129540e1a0ac08971e2f
Aldahir Lazcano <AldahirHL>
2023-05-23 16:14:02 -0600

    se agrega 5FunMayorEdad

---
+++ 5 FunMayorEdad/src/pkg5/funmayoredad/FunMayorEdad.java
@@ -0,0 +1,34 @@
+
+package pkg5.funmayoredad;
+
+import java.util.Scanner;
+
+public class FunMayorEdad {
+
+
+    public static void main(String[] args) {
+        
+  //Declaracion de datos 
+        Scanner Entrada = new Scanner (System.in);
+        int numero;
+        //procesamiento de datos 
+        System.out.print(" ingresa tu edad ");
+        numero = Entrada.nextInt();
+        //Salida de datos 
+        System.out.println(retornaEdad(numero));
+        
+    }
+    
+    public static String retornaEdad(int numero){
+        
+        if (numero >=18){
+            System.out.println("Eres mayor de edad ");
+        } else if (numero<18){
+            System.out.println("Eres menor de edad ");
+        }
+      
+      return "Este es tu caso";   
+
+    }
+    
+}
